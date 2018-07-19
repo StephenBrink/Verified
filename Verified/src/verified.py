7f1d5da933b8bae4a18a55137973fe799ec3d4aa
StephenBrink <Blackxcap@gmail.com>
2018-07-18 23:47:54 -0500

    Initial Commit

---
+++ Verified/src/verified.py
@@ -0,0 +1,27 @@
+from tweepy import OAuthHandler
+from IPython.core.page import page
+from settings import *
+import tweepy, json, sys, random, discord, time
+
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+    
+api = tweepy.API(auth, wait_on_rate_limit=True)
+user = api.get_user('hanaflorencia')
+followers = []
+
+print(user)
+
+if user._json['verified']:
+    print(user._json['name'] + ' is verified!')
+else:
+    print(user._json['name'] + ' is not verified')\
+
+for page in tweepy.Cursor(api.followers, screen_name="hanaflorencia").pages():
+    followers.extend(page)
+    #time.sleep(60)
+    
+print(len(followers))
+
+        
